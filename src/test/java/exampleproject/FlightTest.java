a7e1a4e18e704b665b226826a20c527aec6e76eb
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-03-22 11:17:24 +0100

    added test for flight class

--- src/test/java/exampleproject/FlightTest.java
+++ src/test/java/exampleproject/FlightTest.java
@@ -13,9 +13,10 @@ public class FlightTest {
     @Test
     @DisplayName("Checking for correct constructor")
     public void TestConstructor() {
-        Flight flight = new Flight("Oslo", "Trondheim", "SAS", "13.10", "SK2939");
-
+        Flight flight = new Flight("Oslo", "Trondheim", "13.10", "SK2939");
         
+
+
 
     }
 
