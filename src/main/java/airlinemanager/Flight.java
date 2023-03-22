eb57c4e878fd83b784bf34b28f0946ff87fc8ce6
Christopher Høe <christopher.hoee@gmail.com>
2023-03-22 20:52:00 +0100

    Moved flightnumber logic, tried fixing dest and start issue

--- src/main/java/airlinemanager/Flight.java
+++ src/main/java/airlinemanager/Flight.java
@@ -7,16 +7,17 @@ import java.util.List;
 import java.util.Random;
 
 
-public class Flight extends FlightStored implements Comparable<Flight> {
-    private String destination;
+public class Flight extends FlightStored {
+    // private String destination;
 
-    private String start;
-    private String time;
-    private String flightNumber;
+    // private String start;
+    // private String time;
+    // private String flightNumber;
+
+    private FlightStored flightStored;
 
     public Flight() {
-        super();
-
+        flightStored = new FlightStored();
         this.flightNumber = setFlightNumber();
     }
 
@@ -32,58 +33,11 @@ public class Flight extends FlightStored implements Comparable<Flight> {
         return this.time;
     }
 
-    public String setFlightNumber() {
-
-        Random random = new Random();
-
-        String letterPart = "OY";
-        int numberPart = random.nextInt(3000, 6000);
-
-        String flightNumber = letterPart + String.valueOf(numberPart);
-
-        return flightNumber;
-
-    }
-    
-
-    @Override
-    public int compareTo(Flight arg0) {
-        if (this.timeStringToInt() > arg0.timeStringToInt()) {
-            return 1;
-
-        } else if (this.timeStringToInt() < arg0.timeStringToInt()) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
 
     public static void main(String[] args) {
 
         List<Flight> flights = new ArrayList<>();
        
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-        flights.add(new Flight(null, null, null, "OY"));
-
-        for (Flight flight : flights) {
-            flight.timeStringToInt();
-            
-        }
-
-        System.out.println(flights);
-        Collections.sort(flights);
-
-        // System.out.println(flights);
        
 
 
