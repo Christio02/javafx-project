feafc7894b1f41d5bceddcc3b09170cccc50baae
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-03-23 15:10:41 +0100

    Finished constructor test for flight.java

--- src/test/java/exampleproject/FlightTest.java
+++ src/test/java/exampleproject/FlightTest.java
@@ -21,6 +21,9 @@ public class FlightTest {
 
         String flightNumber = flight.getFlightNumber();
 
+        String dest = flight.getDestination();
+        String start = flight.getStart();
+
         String time = flight.getTime();
 
         assertNotEquals(flight.getStart(), flight.getDestination(), "Start and destination should not be the same!");
@@ -30,11 +33,17 @@ public class FlightTest {
         // checks if there are only numbers after 'OY'
         Pattern pattern = Pattern.compile("^OY\\d{4}$"); // using pattern, that implies 4 digits after 'OY'
 
-        assertTrue(pattern.matcher(flightNumber).matches());
+        assertTrue(pattern.matcher(flightNumber).matches(), "FlightNumber should only contain numbers after 'OY'!");
               
 
         // time checker
 
+        Pattern pattern2 = Pattern.compile("^\\d{2}:\\d{1}0$"); // this pattern cheks if there ate 2 digits, then ':', then 1 digit and then 0
+        // e.g. 10:20 is correct, but 10:21, is not correct
+        assertTrue(pattern2.matcher(time).matches(), "Time should be in this format!: 'xx:x0'");
+
+        // test if start is not equal tod destination
+        assertNotEquals(dest, start, "Start and end should not be the same!");
         
     }
 
