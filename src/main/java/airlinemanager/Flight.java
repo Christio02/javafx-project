79e4365dad961ee49d126d2f29e1fa16618de141
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-03-23 15:09:59 +0100

    deleted som main sysouts

--- src/main/java/airlinemanager/Flight.java
+++ src/main/java/airlinemanager/Flight.java
@@ -1,10 +1,5 @@
 package airlinemanager;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
 
 
 public class Flight extends FlightStored {
