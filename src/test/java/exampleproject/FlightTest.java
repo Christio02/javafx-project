ef324e51338419184cf2c821166c84974b9bd28e
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-03-23 11:49:46 +0100

    Completed test for flightnumber

--- src/test/java/exampleproject/FlightTest.java
+++ src/test/java/exampleproject/FlightTest.java
@@ -2,6 +2,8 @@ package exampleproject;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.regex.Pattern;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,19 +18,24 @@ public class FlightTest {
     @DisplayName("Checking for correct constructor")
     public void TestConstructor() {
         Flight flight = new Flight();
+
+        String flightNumber = flight.getFlightNumber();
+
+        String time = flight.getTime();
+
         assertNotEquals(flight.getStart(), flight.getDestination(), "Start and destination should not be the same!");
         
-        assertEquals("OY", flight.getFlightNumber().substring(0, 2), "Flightnumber letters should be 'OY'!");
+        assertEquals("OY", flightNumber.substring(0, 2), "Flightnumber letters should be 'OY'!");
         
         // checks if there are only numbers after 'OY'
-        for (int i = 0; i < flight.getFlightNumber().substring(2, flight.getFlightNumber().length()).length(); i++) {
-            Character c = flight.getFlightNumber().charAt(i);
+        Pattern pattern = Pattern.compile("^OY\\d{4}$"); // using pattern, that implies 4 digits after 'OY'
 
-            assertEquals(true, Character.isDigit(c));
-
-        }
-
+        assertTrue(pattern.matcher(flightNumber).matches());
               
+
+        // time checker
+
+        Pattern pattern2 = Pattern.compile("")
         
     }
 
