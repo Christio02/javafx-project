2582e4cc8d7373f2db997087aec018a751c8b386
Christopher Høe <christopher.hoee@gmail.com>
2023-03-23 17:18:37 +0100

    Edited comparator

--- src/main/java/airlinemanager/FlightTimeComparator.java
+++ src/main/java/airlinemanager/FlightTimeComparator.java
@@ -1,17 +1,21 @@
 package airlinemanager;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 
 public class FlightTimeComparator implements Comparator<Flight> {
 
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH.mm"); // specifies custom format for parsing the time string
+
     @Override
     public int compare(Flight flight1, Flight flight2) {
 
-        LocalDateTime dateTime1 = LocalDateTime.parse(flight1.getTime());
-        LocalDateTime dateTime2 = LocalDateTime.parse(flight2.getTime());
+        LocalTime time1 = LocalTime.parse(flight1.getTime(), TIME_FORMATTER); // formats each getTime
+        LocalTime time2 = LocalTime.parse(flight2.getTime(), TIME_FORMATTER);
 
-        return dateTime1.compareTo(dateTime2);
+        return time1.compareTo(time2); // compares
 
       
     }
