0c2da46ca8875db7a041d53289433dcb4c8d0d16
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-03-28 14:49:29 +0200

    Added popup window, change styling for app

--- src/main/java/airlinemanager/AirlineController.java
+++ src/main/java/airlinemanager/AirlineController.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -39,20 +43,53 @@ public class AirlineController {
         
         
         listOfFlights.getItems().addAll(flights);
-
-        // makes sure that user 
+    
+    
 
     }
 
     @FXML
     public void bookFlight() { 
         List<Flight> chosen = listOfFlights.getSelectionModel().getSelectedItems(); // gets selected flight
-        this.file.addFlight(chosen);
+        // this.file.addFlight(chosen);
 
-        this.file.writeToFile("testbooking1.txt");
+        // this.file.writeToFile("testbooking1.txt");
+
+        if (chosen.isEmpty()) {
+            // Show error message if no flight is selected
+            Alert errorAlert = new Alert(AlertType.ERROR);
+            errorAlert.setHeaderText("No flight selected");
+            errorAlert.setContentText("Please select a flight before booking.");
+            errorAlert.showAndWait();
+
+        } else {
+            // Show confirmation dialog with flight information
+            Alert confirmationAlert = new Alert(AlertType.CONFIRMATION);
+            confirmationAlert.setHeaderText("Book Flight");
+            confirmationAlert.setContentText("Are you sure you want to book the following flight(s)?\n\n" + chosen.toString());
+            
+            // Create buttons for the dialog
+            ButtonType downloadButton = new ButtonType("Download booking");
+            ButtonType cancelButton = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
+            confirmationAlert.getButtonTypes().setAll(downloadButton, cancelButton);
+            
+            // Handle button actions
+            Optional<ButtonType> result = confirmationAlert.showAndWait();
+            if (result.isPresent() && result.get() == downloadButton) {
+                // Download booking to file
+                WriteBookingToFile file = new WriteBookingToFile();
+                this.file.addFlight(chosen);
+                this.file.writeToFile("booking.txt");
+            }
+        }
 
     }
 
    
 
+
+
 }
+
+
+
--- src/main/java/airlinemanager/App.java
+++ src/main/java/airlinemanager/App.java
@@ -1,8 +1,15 @@
 package airlinemanager;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 
@@ -23,6 +30,7 @@ public class App extends Application {
         // primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/App.fxml"))));
         scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
         primaryStage.setScene(scene);
+
         
         primaryStage.show();
     }
