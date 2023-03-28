a06acbd6f272101b64d62c5127d01fa48b42e150
B1uskyy <mats.kvanvik@hotmail.com>
2023-03-28 15:21:37 +0200

    did small stylechanges to the header1

--- src/main/java/airlinemanager/FlightStored.java
+++ src/main/java/airlinemanager/FlightStored.java
@@ -23,10 +23,8 @@ public class FlightStored {
                 "Bodø", "Malaga", "London", "København", "Stockholm", "Paris", "Barcelona"
         };
 
-        int index; // initzialize index
-
         String location1, location2;
-        
+
         location1 = locations[random.nextInt(locations.length)];
         do {
             location2 = locations[random.nextInt(locations.length)];
@@ -51,7 +49,6 @@ public class FlightStored {
 
         int randomTimeHour = random.nextInt(upperBoundHour - lowerBoundHour + 1) + lowerBoundHour;
         int randomTimeMinutes = random.nextInt(upperBoundMinutes - lowerBoundMinutes + 1) * 10 + lowerBoundMinutes;
-
 
         String intHour = String.format("%02d", randomTimeHour);
         String intMin = String.format("%02d", randomTimeMinutes);
@@ -103,12 +100,11 @@ public class FlightStored {
 
     @Override
     public String toString() {
-       return "Flight: " + flightNumber + "\n" +
-            "From: " + start + "\n" +
-            "To: " + destination + "\n" +
-            "Departure Time: " + getTime();
-}
-
+        return "Flight: " + flightNumber + "\n" +
+                "From: " + start + "\n" +
+                "To: " + destination + "\n" +
+                "Departure Time: " + getTime();
+    }
 
     public static void main(String[] args) {
 
