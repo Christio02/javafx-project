6ac627744349241773887a7f4670ed17f36fd09e
Christopher Høe <christopher.hoee@gmail.com>
2023-03-29 16:01:44 +0200

    removed txt files and edited flight.java, : bookFlight, changed txt file name

--- src/main/java/airlinemanager/Flight.java
+++ src/main/java/airlinemanager/Flight.java
@@ -99,7 +99,7 @@ public class Flight extends FlightStored implements Comparable<Flight> {
     public void bookFlight() {
         this.file.addFlight(this.getFlights()); // adds this flight to file class's list
         
-        this.file.writeToFile("booking2.txt");
+        this.file.writeToFile("testFile.txt");
     }
 
     @Override
