ef158df6b192cff8ce067aa8d33506a8da11e972
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-04-05 13:48:22 +0200

    removed unused imports

--- src/main/java/airlinemanager/Flight.java
+++ src/main/java/airlinemanager/Flight.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Predicate;
+
 
 import airlinemanager.FlightStored;
 import util.FlightAlreadyBookedException;
