3f3cfc6b643bed8cf5495169672bf36a8cb13eb3
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-04-05 19:14:32 +0200

    added documentation for exception classes

--- src/main/java/util/FlightAlreadyBookedException.java
+++ src/main/java/util/FlightAlreadyBookedException.java
@@ -1,8 +1,17 @@
 package util;
 
+/*
+ * This is an excpetion class that extends the RuntimeException class
+ * Its used to add a custom exception for circumstances when a flight is already booked, to prevent user
+ * from booking the same flight twice
+ */
+
 public class FlightAlreadyBookedException extends RuntimeException {
     
 
+    /*
+     * The constructor which is responsible for using the runtimeexcpetion String errmessage class
+     */
     public FlightAlreadyBookedException(String errMessage) {
         super(errMessage);
     }
--- src/main/java/util/FlightNotFoundException.java
+++ src/main/java/util/FlightNotFoundException.java
@@ -1,6 +1,14 @@
 package util;
 
+/*
+ * This is an excpetion class that extends the RuntimeException class
+ * Its used to add a custom exception for circumstances when for example a flight is not initialized or does not exist
+ */
+
 public class FlightNotFoundException extends RuntimeException {
+    /*
+     * The constructor which is responsible for using the runtimeexcpetion String errmessage class
+     */
 
     public FlightNotFoundException(String errMessage) {
         super(errMessage);
