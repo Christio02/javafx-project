4f076cfa4fb07e7f23d3147f8bf9b3f85ef5b6e0
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-04-06 20:36:22 +0200

    added documentation

--- src/main/java/airlinemanager/WriteBookingToFile.java
+++ src/main/java/airlinemanager/WriteBookingToFile.java
@@ -1,5 +1,7 @@
 package airlinemanager;
 
+
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,31 +10,71 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.function.BiConsumer;
+/* 
+ * This class provides functionality for reading and writing flight bookings to and from text files.
+ * Bookings can be added to a list using the addFlight method, and then written to a file using the writeToFile method.
+ * The readFromFile method can be used to read bookings from a file and return them as a string.
+*/
 
 public class WriteBookingToFile {
 
+    // Fields
+
+    /*
+     * This field is setting a protected List, for use in the flight.java class, by adding the booking to this list
+     */
+
     protected List<Flight> flightsToDownload;
+
+    // Constructor
+
+    /*
+     * Initializes a new WriteBookingToFile object with an empty list of flights to download.
+     */
 
     public WriteBookingToFile() {
         this.flightsToDownload = new ArrayList<>();
     }
 
+    /*
+     * This method is responsible for adding the flight object to the list, 
+     * it is called in a bookFlight method in the flight.java class
+     * @param Flight object from flight.java class
+     */
+
     public void addFlight(Flight flight) {
         this.flightsToDownload.add(flight);
     }
+
+    /*
+     * This method is responsible for removing a booking (i.e. canceling the booking)
+     * It is called in a separate method in the flight.java class
+     * @param Flight object to be removed
+     */
 
     public void removeFlight(Flight flight) {
         int index = this.flightsToDownload.indexOf(flight);
         this.flightsToDownload.remove(index);
     }
 
+    /*
+     * A method that returns the current list of flights for download to list
+     * @return List of flight objects
+     */
+
     public List<Flight> getFlightsToDownload() {
         return this.flightsToDownload;
     }
 
-    public void writeToFile(String filename){//, List<Flight> flightsList) {
+    /*
+     * This methdod writes bookings to a textfile using a specified file name 
+     * It will then create a txt file with the name, and then write the flight object
+     * in the list to a toString representation. This representation is a special case, 
+     * for making it easier to read the content from the file and then display it in the application.
+     * @param String with the specified filename
+     */
+
+    public void writeToFile(String filename){
 
         PrintWriter writer = null;
         try {
@@ -52,7 +94,13 @@ public class WriteBookingToFile {
         }     
     }
 
-   
+   /*
+    * This method is used for reading a booking which is stored on a file, it uses the specified filename 
+    * and then utilizes bufferedreader and filereader classes for reading from the file.
+    * It also uses Stringbuilder to build the content from the file to a string format
+    *@param specified filename in string format
+    *@return returns a flight object in string format to be used in application
+    */
 
     public String readFromFile(String filename) {
         
@@ -89,17 +137,7 @@ public class WriteBookingToFile {
 
     }
 
-    // @Override
-    // public String toString() {
-    //     return "Flight: " + flight.flightNumber + "\n" +
-    //     "From: " + flight.start + "\n" +
-    //     "To: " + flight.destination + "\n" +
-    //     "Departure Time: " + flight.getTime();
-        
-    // }
-
    
-
 
 
 
